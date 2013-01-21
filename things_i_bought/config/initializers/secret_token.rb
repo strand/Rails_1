20ba2c82569ac18cafd4b68f76d387f407b46492
B Strand <strand@bettermistak.es>
2013-01-21 11:27:23 -0800

    Generated application following Rails 4 in Action (Beta)

--- things_i_bought/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ThingsIBought::Application.config.secret_token = 'c1c0976443103c413483856a9fe375401195b07b8657178112e5cf7b46b1d99c05b907af13df691deddee1f5197e8715091d79e13ecb5265659962284658f13a'
---
+++ ticketee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'c4943040594f348ca511dec93a8aedd4e59cfc2b2a1c0c8dc93232e3bc049910428ac59e3477c5bdba746ee22633833e8fddf1878fbe7e4f9d0cf665684e101d'
